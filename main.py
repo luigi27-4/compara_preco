78a57b7ba38ee6102709aff37320607037c56242
arthurzanelli <arthurcamilotozanelli@gmail.com>
2026-09-14 18:01:16 -0300

    deletei(nada para ver aqui. Caso tenha que visitar o arquivo do comentario anterior, vá em "comentarios e extras")
    Removed comments about reliable price comparison sites.

--- main.py
+++ main.py
@@ -1,15 +1,1 @@
-#sites confiáveis para busca de preços
 
-#Buscapé: Permite comparar preços de produtos de diversas categorias e oferece cashback e alertas de preço.
-
-
-#Zoom(não o das ligações): Similar ao Buscapé, o Zoom permite comparar preços e configurar alertas para ser notificado quando o preço atingir ou ficar abaixo do valor desejado
-
-
-
-#E-Preços: Analisa preços de dezenas de lojas em tempo real e oferece um algoritmo avançado para encontrar o melhor negóci/o. 
-
-
-
-#kompaia(pegar inpiração, porque tem a mesma ideia): Utiliza IA para comparar preços, cotar serviços e encontrar profissionais e empresas para resolver serviços na sua casa. 
-#Esses sites são confiáveis e oferecem uma variedade de ferramentas para ajudar os usuários a encontrar as melhores ofertas e preços certos para os produtos que desejam comprar.
